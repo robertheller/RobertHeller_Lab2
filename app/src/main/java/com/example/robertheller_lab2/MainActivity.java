3f7688a841d2e2d62fd45f8e8e7ed5410c97e774
Robert <robert_james_heller@yahoo.ca>
2021-01-28 20:53:50 -0800

    Added all functionality.

--- app/src/main/java/com/example/robertheller_lab2/MainActivity.java
+++ app/src/main/java/com/example/robertheller_lab2/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.robertheller_lab2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,29 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void convert_ftoc(View view) {
+        EditText convert_this = (EditText)findViewById(R.id.enter_number);
+        TextView results = (TextView)findViewById(R.id.display_results);
+
+        String get_value = convert_this.getText().toString();
+        double value = Double.parseDouble(get_value);
+
+        double converted_val = (value - 32) * 0.55;
+
+        results.setText(Double.toString(converted_val));
+    }
+
+    public void convert_ctof(View view) {
+        EditText convert_this = (EditText)findViewById(R.id.enter_number);
+        TextView results = (TextView)findViewById(R.id.display_results);
+
+        String get_value = convert_this.getText().toString();
+        double value = Double.parseDouble(get_value);
+
+        double converted_val = (value * 1.8) + 32;
+
+        results.setText(Double.toString(converted_val));
     }
 }
